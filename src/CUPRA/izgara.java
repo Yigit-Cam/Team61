313a0c2ea896c5af755c111771fbd9500d25afab
rftbtr <rifatbatir@gmail.com>
2025-03-03 22:55:53 +0300

    afiyet olsun

---
+++ src/CUPRA/izgara.java
@@ -0,0 +1,9 @@
+package CUPRA;
+
+public class izgara {
+    public static void main(String[] args) {
+        System.out.println("izgara");
+        System.out.println("kumru");
+        System.out.println("boyoz");
+    }
+}
